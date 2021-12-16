16b13110335784c4304f78a4f096c5bda6f442a2
Pascal Mattes <pascal@codeatelier.com>
2021-12-16 09:17:20 +0100

    Add new logo

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,6 +2,8 @@
   <div
     class="absolute z-50 flex items-center justify-center w-full py-8 md:px-24 lg:px-48"
   >
-    <div class="md:w-full max-w-screen-2xl"></div>
+    <div class="md:w-full max-w-screen-2xl">
+      <img src="/img/logo.webp" alt="Logo" />
+    </div>
   </div>
 </template>
