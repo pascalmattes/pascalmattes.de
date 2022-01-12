f56f1dbd5a00694a9d90556d436d8876a01b25e4
Pascal Mattes <pascal.mattes@icloud.com>
2022-01-12 15:35:00 +0100

    Add footer

---
+++ components/TheFooter.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex justify-center px-8 py-16 bg-zinc-800 lg:px-48 sm:px-20">
+    <div class="flex justify-between w-full max-w-screen-lg">
+      <div
+        class="flex flex-col space-y-1 sm:space-x-6 sm:space-y-0 sm:flex-row"
+      >
+        <a
+          href="https://twitter.com/pascalmtts"
+          class="text-xl transition-colors text-white/75 hover:text-white"
+          >Twitter</a
+        >
+        <a
+          href="https://instagram.com/pascalmtts"
+          class="text-xl transition-colors text-white/75 hover:text-white"
+          >Instagram</a
+        >
+        <a
+          href="https://linkedin.com/in/pascalmtts"
+          class="text-xl transition-colors text-white/75 hover:text-white"
+          >LinkedIn</a
+        >
+        <a
+          href="https://github.com/pascalmtts"
+          class="text-xl transition-colors text-white/75 hover:text-white"
+          >GitHub</a
+        >
+      </div>
+      <div>
+        <NuxtLink
+          to="/imprint"
+          class="text-xl transition-colors text-white/75 hover:text-white"
+          >Impressum</NuxtLink
+        >
+      </div>
+    </div>
+  </div>
+</template>
